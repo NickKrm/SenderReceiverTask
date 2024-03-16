6b3abd35b67e18cdbff2d29c188781be94ac9316
NickKrm <nik2.krumin@gmail.com>
2024-03-16 23:07:11 +0200

    fisrt commit

---
+++ Receiver/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Receiver
+{
+    static void Main(string[] args)
+    {
+        const int listenPort = 11001; // Receiver listening on port 11001
+
+        using (UdpClient listener = new UdpClient(listenPort))
+        {
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast...");
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    Console.WriteLine($"Received broadcast from {groupEP} :");
+                    Console.WriteLine($" {Encoding.UTF8.GetString(bytes, 0, bytes.Length)}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ Sender/sender.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Sender
+{
+    static void Main(string[] args)
+    {
+        const int senderPort = 11000; // Sender's port
+        const int receiverPort = 11001; // Receiver's port
+        const string server = "127.0.0.1"; // Target server address
+
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(server), receiverPort);
+        using (UdpClient udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, senderPort)))
+        {
+            Console.WriteLine("Enter a message to send to the receiver. Type 'exit' to quit.");
+            string message;
+            do
+            {
+                message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message) && message.ToLower() != "exit")
+                {
+                    byte[] sendBytes = Encoding.UTF8.GetBytes(message);
+                    udpClient.Send(sendBytes, sendBytes.Length, endPoint);
+                }
+            }
+            while (message != null && message.ToLower() != "exit");
+        }
+    }
+}
